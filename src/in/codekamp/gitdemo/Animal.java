541ade72f4ed0bef4b8d70b644879f150623bd68
Nischay Sinha <go4nishchay@gmail.com>
2016-03-07 18:06:11 +0530

    added animal class

--- src/in/codekamp/gitdemo/Animal.java
+++ src/in/codekamp/gitdemo/Animal.java
@@ -3,13 +3,5 @@ package in.codekamp.gitdemo;
 /**
  * Created by Nischay on 07-03-2016.
  */
-public class Demo {
-
-
-    public static void main(String[] args) {
-
-    }
-
-
-
+public class Animal {
 }
